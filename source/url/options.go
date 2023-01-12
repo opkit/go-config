d20604a4cbd92c1d07b292803cd85b0aa4890526
chunmeng.gao <chunmeng.gao@funplus.com>
2023-01-12 20:16:11 +0800

    add url source

---
+++ source/url/options.go
@@ -0,0 +1,17 @@
+package url
+
+import (
+	"context"
+	"github.com/opkit/go-config/source"
+)
+
+type urlKey struct{}
+
+func WithURL(u string) source.Option {
+	return func(o *source.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, urlKey{}, u)
+	}
+}
---
+++ source/url/watcher.go
@@ -0,0 +1,31 @@
+package url
+
+import (
+	"errors"
+	"github.com/opkit/go-config/source"
+)
+
+type urlWatcher struct {
+	u    *urlSource
+	exit chan bool
+}
+
+func newWatcher(u *urlSource) (*urlWatcher, error) {
+	return &urlWatcher{
+		u:    u,
+		exit: make(chan bool),
+	}, nil
+}
+
+func (u *urlWatcher) Next() (*source.ChangeSet, error) {
+	<-u.exit
+	return nil, errors.New("url watcher stopped")
+}
+
+func (u *urlWatcher) Stop() error {
+	select {
+	case <-u.exit:
+	default:
+	}
+	return nil
+}
